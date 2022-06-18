69a9ba6a13d3f50b573252c0fb3a67fedeff7afc
Horacio Zorola <73261219+horaciozoroladev@users.noreply.github.com>
2022-06-18 00:47:56 -0500

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -4,24 +4,37 @@ use drinks_api;
 
 create table users (
     id int not null auto_increment,
-    _id varchar(100) default uuid(),
-    name varchar(100),
+    _id varchar(100) not null,
+    givenname varchar(100),
     surname varchar(100),
     username varchar(100),
     pass longtext,
     token longtext,
+    
+    primary key(id)
 );
+
+insert into users set 
+	_id = uuid(),
+    givenname = 'john',
+    surname = 'doe',
+    username = 'johndoe',
+    pass = to_base64('12345');
 
 create table done_drinks (
     id int not null auto_increment,
-    _id varchar(100) default uuid(),
+    _id varchar(100) not null,
     idDrink varchar(20),
-    users__id varchar(100)
+    users__id varchar(100),
+    
+    primary key(id)
 );
 
-create table favourite_drinks (
+create table favorite_drinks (
     id int not null auto_increment,
-    _id varchar(100) default uuid(),
+    _id varchar(100) not null,
     idDrink varchar(20),
-    users__id varchar(100)
+    users__id varchar(100),
+    
+    primary key(id)
 );
